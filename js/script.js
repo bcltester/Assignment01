30ef9ddddc97b7de33e65250cd5c65fba88ec568
bcltester <bcltester@users.noreply.github.com>
2021-02-17 20:11:21 -0800

    completed 644 Assignment 1

---
+++ js/script.js
@@ -0,0 +1,125 @@
+// STEP 1
+
+// let someMonth;
+// function theMonth() {
+// }
+// let currentMonth;
+// let summerMonth;
+// let myLibraryFunction;
+
+// STEP 2
+/*
+// numeric literal expression
+5
+//string literal expression
+"hello world"
+// boolean literal expression
+true
+//null literal expression
+null
+*/
+
+// STEP 3
+// x = 4 + 8
+// let string = "I am a dog."
+
+// STEP 4
+// let strFirstName, strLastName, strAddress, strCity, strState, numZipCode, numYourAge, strReferralSource, strMayWeContactYou;
+
+// STEP 5
+// // Method 1
+// let strFirstName;
+// strFirstName = "Jack";
+
+// //Method 2
+// let strLastName = "Sparrow";
+
+// //Method 3
+// const numYourAge = 19;
+
+// STEP 6
+// let variable;
+// variable = 8 + "mile";
+// console.log(variable);
+
+// STEP 7
+// let x, y;
+// x = false + "hood";
+// y = true + 1;
+// console.log(x);
+// console.log(y);
+
+// STEP 8
+// No, not valid. The apostrophe in I'm must be escaped
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."';
+// window.console.log(someString);
+
+// STEP 9
+// let x = null;
+// let y;
+// console.log(x);
+// console.log(y);
+
+// STEP 10
+// console.log(typeof "hello");
+// console.log(typeof 4);
+// console.log(typeof true);
+// console.log(typeof new Number()); //object is anything that doesn't fall in the other typeof categories
+// console.log(typeof [1,2,3]); //object is anything that doesn't fall in the other typeof categories, like this array
+// console.log(typeof variable);
+
+// STEP 11
+// alert("Hello" + " Steve Jobs" + ", welcome to the JavaScript class!");
+
+// STEP 12
+// let name = "Steve Jobs";
+// alert("Hello " + name + ", welcome to the JavaScript class!");
+
+// STEP 13
+// let course = "JavaScript";
+// let name = "Steve Jobs";
+// alert("Hello " + name + ", welcome to the " + course + " class!");
+
+// STEP 14
+// let course = "JavaScript";
+// let name = "Steve Jobs";
+// alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+
+// STEP 15
+// let course = "JavaScript";
+// let name = prompt("Enter your name:", "Write your name here");
+// alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+
+// STEP 16
+// let course = prompt("Enter the course name:");
+// let name = prompt("Enter your name:", "Write your name here");
+// alert("Hello " + name + ".\nWelcome to the " + course + " class!");
+
+// STEP 17
+// let x = 10;
+// let y = 20;
+// console.log(x + y);
+
+// STEP 18
+// let x = 20;
+// console.log(x += 20);
+
+// STEP 19
+// let x = 20;
+// x *= 5;
+// console.log(x);
+// // console.log(x *= 5);  // abbreviated way to do it
+
+// STEP 20
+// let x = 20 % 3;
+// console.log(x /= 1);
+
+// STEP 21
+// let x = 10;
+// let y = 20;
+// console.log(x > 5 || y < 100);
+
+// STEP 22
+// let x = 10;
+// let y = 20;
+// console.log(x <= 5 && y > 5);
